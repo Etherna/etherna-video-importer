c48f2c757c90fa7f9638c382ff567f25c4d0e51c
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-07-02 10:04:34 +0200

    EVI-121 PrintFFmpegEncodeInfo

--- src/EthernaVideoImporter.Core/Services/EncoderService.cs
+++ src/EthernaVideoImporter.Core/Services/EncoderService.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Etherna.VideoImporter.Core.Services
@@ -52,19 +53,12 @@ namespace Etherna.VideoImporter.Core.Services
             activedCommands.Add(command);
             Console.CancelKeyPress += new ConsoleCancelEventHandler(ManageInterrupted);
 
+            var regex = new Regex(@"time=([\d:.]+)|speed=([\d.]+x)");
             // Print filtered console output.
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             Task.Run(() =>
             {
-                /*
-                 * WTF! It works as an async method.
-                 * The enumerable is built on top of a "while(true)" that ends when the process is stopped.
-                 */
-                foreach (var line in command.GetOutputAndErrorLines())
-                {
-                    if (line.StartsWith("frame=", StringComparison.InvariantCulture))
-                        Console.Write(line + '\r');
-                }
+                PrintFFmpegEncodeInfo(command, regex);
             });
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
@@ -179,5 +173,42 @@ namespace Etherna.VideoImporter.Core.Services
 
         private void ManageInterrupted(object? sender, ConsoleCancelEventArgs args) =>
             activedCommands.ForEach(c => c.Kill());
+
+        private static void PrintFFmpegEncodeInfo(Command command, Regex regex)
+        {
+            /*
+            * WTF! It works as an async method.
+            * The enumerable is built on top of a "while(true)" that ends when the process is stopped.
+            */
+            int rowsStart = Console.CursorTop;
+            foreach (var line in command.GetOutputAndErrorLines())
+            {
+                // Write info line.
+                if (line.StartsWith("frame=", StringComparison.InvariantCulture))
+                {
+                    MatchCollection matches = regex.Matches(line);
+                    if (matches.Count == 2)
+                    {
+                        // Clean the old lines info.
+                        for (int i = 0; i < Console.CursorTop - rowsStart + 1; i++)
+                        {
+                            Console.SetCursorPosition(0, rowsStart + i);
+                            Console.Write(new string(' ', Console.WindowWidth));
+                            Console.Write('\r');
+                        }
+
+                        // Write new line updated.
+                        foreach (Match match in matches)
+                        {
+                            if (match.Groups[1].Success)
+                                Console.Write($"Time: {match.Groups[1].Value}\t");
+                            else if (match.Groups[2].Success)
+                                Console.Write($"Speed: {match.Groups[2].Value}\t");
+                        }
+                    }
+                    Console.Write('\r');
+                }
+            }
+        }
     }
 }
