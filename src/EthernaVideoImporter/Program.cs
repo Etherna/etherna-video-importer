a2d0a1cd8881b1c4f95cf0f161978acf0d69eb35
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-07-03 17:41:59 +0200

    EVI-119 refactory

--- src/EthernaVideoImporter/Program.cs
+++ src/EthernaVideoImporter/Program.cs
@@ -295,7 +295,7 @@ namespace Etherna.VideoImporter
                 return;
 
             // Check for ffmpeg
-            var selectedFFMpegFolderPath = await FFmpegUtility.CheckAndGetAsync(customFFMpegFolderPath);
+            var selectedFFMpegFolderPath = await FFmpegUtility.FFmpegCheckAndGetAsync(customFFMpegFolderPath);
             if (selectedFFMpegFolderPath is null)
             {
                 Console.WriteLine("FFmpeg not found");
