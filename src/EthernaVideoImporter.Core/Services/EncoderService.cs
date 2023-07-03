ae0114618976a5f722823c8c6166ce98bfee9f27
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-07-03 15:27:48 +0200

    fix

--- src/EthernaVideoImporter.Core/Services/EncoderService.cs
+++ src/EthernaVideoImporter.Core/Services/EncoderService.cs
@@ -57,7 +57,7 @@ namespace Etherna.VideoImporter.Core.Services
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             Task.Run(() =>
             {
-                PrintFFmpegEncodeInfo(command, regex);
+                PrintFFmpegEncodeInfo(command);
             });
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
