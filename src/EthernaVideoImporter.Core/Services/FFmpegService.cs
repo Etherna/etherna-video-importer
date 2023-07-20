1576fd6ee187ad5bee0aab4e5bea8f2b0be8a580
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-07-20 19:54:09 +0200

    EVI-121 fix merge conflict

--- src/EthernaVideoImporter.Core/Services/FFmpegService.cs
+++ src/EthernaVideoImporter.Core/Services/FFmpegService.cs
@@ -9,6 +9,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Etherna.VideoImporter.Core.Services
@@ -53,15 +54,7 @@ namespace Etherna.VideoImporter.Core.Services
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             Task.Run(() =>
             {
-                /*
-                 * WTF! It works as an async method.
-                 * The enumerable is built on top of a "while(true)" that ends when the process is stopped.
-                 */
-                foreach (var line in command.GetOutputAndErrorLines())
-                {
-                    if (line.StartsWith("frame=", StringComparison.InvariantCulture))
-                        Console.Write(line + '\r');
-                }
+                PrintFFmpegEncodeInfo(command);
             });
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
@@ -193,5 +186,43 @@ namespace Etherna.VideoImporter.Core.Services
 
         private void ManageInterrupted(object? sender, ConsoleCancelEventArgs args) =>
             activedCommands.ForEach(c => c.Kill());
+
+        private static void PrintFFmpegEncodeInfo(Command command)
+        {
+            /*
+            * WTF! It works as an async method.
+            * The enumerable is built on top of a "while(true)" that ends when the process is stopped.
+            */
+            int rowsStart = Console.CursorTop;
+            var regex = new Regex(@"time=([\d:.]+)|speed=([\d.]+x)");
+            foreach (var line in command.GetOutputAndErrorLines())
+            {
+                // Write info line.
+                if (line.StartsWith("frame=", StringComparison.InvariantCulture))
+                {
+                    MatchCollection matches = regex.Matches(line);
+                    if (matches.Count == 2)
+                    {
+                        // Clean the old lines info.
+                        for (int i = 0; i < Console.CursorTop - rowsStart + 1; i++)
+                        {
+                            Console.SetCursorPosition(0, rowsStart + i);
+                            Console.Write(new string(' ', Console.WindowWidth));
+                            Console.Write('\r');
+                        }
+
+                        // Write new line updated.
+                        foreach (Match match in matches)
+                        {
+                            if (match.Groups[1].Success)
+                                Console.Write($"Time: {match.Groups[1].Value}\t");
+                            else if (match.Groups[2].Success)
+                                Console.Write($"Speed: {match.Groups[2].Value}\t");
+                        }
+                    }
+                    Console.Write('\r');
+                }
+            }
+        }
     }
 }
