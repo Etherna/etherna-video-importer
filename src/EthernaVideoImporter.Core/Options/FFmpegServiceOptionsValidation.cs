e225bbaa629abfaa9fd9e92fc2ef66f48fc9295c
Federico Cicciarella <federico.cicciarella@gmail.com>
2023-07-22 14:48:27 +0200

    EVI-105 fix text

--- src/EthernaVideoImporter.Core/Options/FFmpegServiceOptionsValidation.cs
+++ src/EthernaVideoImporter.Core/Options/FFmpegServiceOptionsValidation.cs
@@ -10,7 +10,7 @@ namespace Etherna.VideoImporter.Core.Options
             if (string.IsNullOrWhiteSpace(options.FFmpegFolderPath))
                 return ValidateOptionsResult.Fail($"FFmpeg folder path can't be null or white spaces");
             if (!FFmpegServiceOptions.PresetCodecs.Any(pc => pc == options.PresetCodec))
-                return ValidateOptionsResult.Fail($"{options.PresetCodec} it'sn an allowed value.");
+                return ValidateOptionsResult.Fail($"{options.PresetCodec} it is not an allowed value.");
 
             return ValidateOptionsResult.Success;
         }
